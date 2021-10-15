f7c3f0d1109f619b839939e0dfce410c3cacf71e
wesley.cardoso <wesley.cardoso@gazin.com.br>
2021-10-14 21:20:26 -0300

    feat: JSON data and show instance

---
+++ Main.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DeriveGeneric, TupleSections, LambdaCase #-}
+
+module JSONParser where
+
+import Control.Applicative
+import Control.Monad
+import Data.Bits
+import Data.Char
+import Data.Functor
+import Data.List
+import GHC.Generics
+import Numeric 
+import Test.QuickCheck hiding (Positive, Negative)
+
+data JValue = JNull 
+    | JBool Bool 
+    | JString String
+    | JNumber { int :: Integer, frac :: [Int], exponent :: Integer}
+    | JArray [JValue]
+    | JObject [(String, JValue)]
+    deriving (Eq, Generic)
+
+instance Show JValue where 
+    show value = case value of
+        JNull        -> "null"
+        JBool True   -> "true"
+        JBool False  -> "false"
+        JString s    -> showJSONString s
+        JNumber s [] 0 -> show s
+        JNumber s [] e -> show s ++ "e" ++ show e
+        JNumber s f 0 -> show s ++ "." ++ concatMap show f 
+        JNumber s f e -> show s ++ "." ++ concatMap show f ++ "e" ++ show e
+        JArray a      -> "[" ++ intercalate ", " (map show a) ++ "]"
+        JObject o     -> "{" ++ intercalate "," (map showKV o) ++ "}"
+        where 
+            showKV (k,v) = showJSONString k ++ ": " ++ show v
+
+showJSONString :: String -> String
+showJSONString s = "\"" ++ concatMap showJSONChar s ++ "\""
+
+--  haskell is control is different from json control values
+isCharControl :: Char -> Bool
+isCharControl c = c `elem` ['\0' .. '\31']
+
+showJSONChar :: Char -> String
+showJSONChar c = case c of 
+  '\'' -> "'"
+  '\"' -> "\\\""
+  '\\' -> "\\\\"
+  '/'  -> "\\/"
+  '\b' -> "\\b"
+  '\f' -> "\\f"
+  '\n' -> "\\n"
+  '\r' -> "\\r"
+  '\t' -> "\\t"
+  _ | isCharControl c -> "\\u" ++ showJSONNonASCIIChar c
+  _ -> [c]
+  where
+    showJSONNonASCIIChar c =
+      let a = "0000" ++ showHex (ord c) "" in drop (length a - 4) a
